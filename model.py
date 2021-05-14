23e1574d07fcc81d2d6016ae38850fdb1be0fefd
moris <44795768+morimoris@users.noreply.github.com>
2021-05-14 11:28:43 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
-import numpy as np
 from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Conv2D, Input, Conv2DTranspose, Concatenate, Lambda, Average
+from tensorflow.python.keras.layers import Conv2D, Input, Concatenate, Lambda, Average
 
 def ESPCN(input_list, input_channels, mag):
     if len(input_list) == 1:
@@ -30,8 +29,8 @@ def ESPCN(input_list, input_channels, mag):
     return pixel_shuffle
 
 def RVSR(input_LR_num, input_channels, mag):
-    input_list = []
-    output_list = []
+    input_list = input_LR_num * [None]
+    output_list = (input_LR_num // 2 + 1) * [None]
 
     for img in range(input_LR_num): 
         input_list.append(Input(shape = (None, None, input_channels), name = "input_" + str((img))))
